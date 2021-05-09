b15c871c7bb33ff8ded30b0e5ac6f92103de8107
Shawn Krisman <krisman.shawn@gmail.com>
2021-05-09 11:10:01 -0700

    finished prime function.

--- x64/src/3/problem-3.asm
+++ x64/src/3/problem-3.asm
@@ -1,17 +1,53 @@
-; ----------------------------------------------------------------------------------------
-; Writes "Hola, mundo" to the console using a C library. Runs on Linux.
-;
-;     nasm -felf64 hola.asm && gcc hola.o && ./a.out
-; ----------------------------------------------------------------------------------------
+; RDI, RSI, RDX, RCX, R8, and R9 -> RAX, RDX
+; XMM0 to XMM7 -> XMM0, XMM1
 default rel
 
-          global    main
-          extern    puts
+global    main
+extern    printf
 
-          section   .text
-main:                                       ; This is called by the C library startup code
-          mov       rdi, message            ; First integer (or pointer) argument in rdi
-          call      puts wrt ..plt                    ; puts(message)
-          ret                               ; Return from main back into C library wrapper
+section   .text
+main:
+    mov rbp, rsp
+    mov r12, 1
+.loop:
+    mov  rsp, rbp
+    inc  r12
+    mov  rdi, r12
+    call isprime
+    mov  rdi, format
+    mov  rsi, r12
+    mov  rdx, rax
+    mov  rax, 0
+    call printf wrt ..plt
+    jmp  .loop
+
+isprime:
+    push rbp
+    mov  rbp, rsp
+    mov  rcx, rdi
+    shr  rcx, 1
+    mov  rbx, 1
+.loop:
+    inc  rbx
+    cmp  rbx, rcx
+    jg	 .isprime
+    xor  rdx, rdx
+    mov  rax, rdi
+    div  rbx
+    cmp  rdx, 0
+    je   .isnotprime
+    jmp  .loop
+.isprime:
+    mov  rax, 1
+    jmp  .finish
+.isnotprime:
+    mov  rax, 0
+.finish:
+    mov rbp, rsp
+    pop rbp
+    ret
+
+
+section .data
 message:
-          db        "Hola, mundo", 0        ; Note strings must be terminated with 0 in C
+    format    db `%u: %u\n`, 0
